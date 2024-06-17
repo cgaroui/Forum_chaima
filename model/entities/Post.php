1235a99c9a4e91d5c5aa9767766aeac5dd5578a4
chaimagaroui2@gmail.com <chaimagaroui2@gmail.com>
2024-06-17 16:20:52 +0200

    ajout: entitée post et entité user

---
+++ model/entities/Post.php
@@ -0,0 +1,123 @@
+<?php
+namespace Model\Entities;
+
+use App\Entity;
+
+final class User extends Entity{
+
+    private $id;
+    private $user;
+    private $topic;
+    private $creationDate;
+    private $text;
+   
+
+
+    public function __construct($data){         
+        $this->hydrate($data);        
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user
+     */ 
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the value of user
+     *
+     * @return  self
+     */ 
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of topic
+     */ 
+    public function getTopic()
+    {
+        return $this->topic;
+    }
+
+    /**
+     * Set the value of topic
+     *
+     * @return  self
+     */ 
+    public function setTopic($topic)
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of creationDate
+     */ 
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * Set the value of creationDate
+     *
+     * @return  self
+     */ 
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of text
+     */ 
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set the value of text
+     *
+     * @return  self
+     */ 
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function __toString(){
+        $this->text = $text;
+    }
+}
--- model/entities/User.php
+++ model/entities/User.php
@@ -11,6 +11,11 @@ final class User extends Entity{
 
     private $id;
     private $nickName;
+    private $password;
+    private $role;
+    private $creationDate;
+    private $email;
+
 
     public function __construct($data){         
         $this->hydrate($data);        
@@ -51,6 +56,87 @@ final class User extends Entity{
         return $this;
     }
 
+    
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        
+        return $this;
+    }
+    
+    /**
+     * Get the value of role
+     */ 
+    public function getRole()
+    {
+        return $this->role;
+    }
+    
+    /**
+     * Set the value of role
+     *
+     * @return  self
+     */ 
+    public function setRole($role)
+    {
+        $this->role = $role;
+        
+        return $this;
+    }
+    
+    /**
+     * Get the value of creationDate
+     */ 
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+    
+    /**
+     * Set the value of creationDate
+     *
+     * @return  self
+     */ 
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = $creationDate;
+        
+        return $this;
+    }
+
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
     public function __toString() {
         return $this->nickName;
     }
