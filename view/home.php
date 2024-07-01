b336609e8dfcaab7431820e7e4d71000dc6b692f
chaima garoui <chaimagaroui2@gmail.com>
2024-07-01 12:01:27 +0200

    ajout : css navbar

--- view/home.php
+++ view/home.php
@@ -1,10 +1,20 @@
-<h1>BIENVENUE SUR LE FORUM</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>home</title>
+</head>
+<body>
+    
+</body>
+</html>
+<h2>BIENVENUE SUR LE FORUM</h2>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam,
-     itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+<p>Discutez de choses et sujets qui vous interessent</p>
 
 <p>
-    <a href="index.php?ctrl=security&action=login">Se connecter</a><br>
-    <a href="index.php?ctrl=security&action=register">S'inscrire</a>
-    
+    <a href="index.php?ctrl=security&action=login" class="btn-accueil">Se connecter</a><br>
+    <a href="index.php?ctrl=security&action=register" class="btn-accueil">S'inscrire</a>
 </p>
--- view/layout.php
+++ view/layout.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-        <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
+        <link rel="stylesheet" href="<?= PUBLIC_DIR ?>css/style.css">
         <title>FORUM</title>
     </head>
     <body>
@@ -17,34 +17,28 @@
                 <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
                 <h3 class="message" style="color: green"><?= App\Session::getFlash("success") ?></h3>
                 <header>
-                    <nav>
-                        <div id="nav-left">
-                            <a href="index.php?ctrl=home&action=index">Accueil</a>
-                            <?php
-                            if(App\Session::isAdmin()){
-                                ?>
-                                <!-- <a href="index.php?ctrl=home&action=users">Voir la liste des gens</a> -->
-                            <?php } ?>
-                        </div>
-                        <div id="nav-right">
+                    <nav class="nav-bar">
+                     
                         <?php
                             // si l'utilisateur est connecté 
-                            if(App\Session::getUser()){
-                                ?>
-                                <a href="index.php?ctrl=security&action=profile"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
+                            if(App\Session::getUser()){?>
+                                <div class="profil"><a href="index.php?ctrl=security&action=profile"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a></div>
+                                 <div id="nav-right">
+                                <a href="index.php?ctrl=home&action=index">Accueil</a>
                                 <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
                                 <a href="index.php?ctrl=forum&action=index">Liste des catégories</a>
-                                <?php
-                            }
-                            else{
-                                ?>
+                                </div>
+                           <?php }
+                            else{?>
+                                <div id="nav-right">
+                                <a href="index.php?ctrl=home&action=index">Accueil</a>
                                 <a href="index.php?ctrl=security&action=login">Connexion</a>
                                 <a href="index.php?ctrl=security&action=register">Inscription</a>
                                 <a href="index.php?ctrl=forum&action=index">Liste des catégories</a>
-                            <?php
+                                </div><?php
                             }
                         ?>
-                        </div>
+                        
                     </nav>
                 </header>
                 
