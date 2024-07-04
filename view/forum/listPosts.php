8af72e3cee720e793e752ea00532488dc1594ca2
garoui chaima <chaimagaroui2@gmail.com>
2024-07-04 11:18:07 +0200

    ajout : responsive

--- view/forum/listPosts.php
+++ view/forum/listPosts.php
@@ -12,60 +12,42 @@
     <div class="tous-post">
         <?php foreach($posts as $post) { 
             $userId = App\Session::getUser() ? App\Session::getUser()->getId() : null;
-            $role = App\Session::getUser() ? App\Session::getUser()->getRole() : null;
-            if($userId){ ?>
-                <div class="un-post">
-                    <!-- photo de profil rond + pseudo en dessous -->
-                    <div class="post-profil">
-                        <div class="post-img">
-                            <img src="profil.png" alt="photo de profil">
-                        </div>
-                        <?="\n" .$post->getUser() ?><br>
+            $role = App\Session::getUser() ? App\Session::getUser()->getRole() : null; ?>
+            
+            <div class="un-post">
+                <div class="post-profil">
+                    <div class="post-img">
+                        <img src="profil.png" alt="photo de profil">
                     </div>
+                    <?= "\n" . $post->getUser() ?><br>
+                </div>
 
-                    <div class="contenu-post">
-                        le <?= $post->getCreationDate()->format("d/m/Y H:i") ?>
-                        <div class="text-post">
-                            <?=$post->getText()?><br>
-                        </div>
+                <div class="contenu-post">
+                    le <?= $post->getCreationDate()->format("d/m/Y H:i") ?>
+                    <div class="text-post">
+                        <?= $post->getText() ?><br>
                     </div>
-                    
+                </div>
+
+                <?php if($userId && ($role == "admin" || $post->getUser()->getId() == $userId)) { ?>
                     <div class="supp-post">
-                    <?php if($role == "admin" || $post->getUser()->getId() == $userId) { ?>
-                        <a href="index.php?ctrl=forum&action=supprimerPost&id=<?= $post->getId()?>">supprimer</a><br>
-                    <?php } ?>
+                        <a href="index.php?ctrl=forum&action=supprimerPost&id=<?= $post->getId() ?>">supprimer</a><br>
                     </div>
-                </div>    
-            <?php } else { ?>
-                <div class="un-post">
-                    <div class="post-profil">
-                        <div class="post-img">
-                            <img src="profil.png" alt="photo de profil">
-                        </div>
-                        <?="\n" .$post->getUser() ?><br>
-                    </div>
-
-                    <div class="contenu-post">
-                        le <?= $post->getCreationDate()->format("d/m/Y H:i") ?>
-                        <div class="text-post">
-                            <?=$post->getText()?><br>
-                        </div>
-                    </div>
-                </div>    
-            <?php } ?>
+                <?php } ?>
+            </div>
         <?php } ?>
     </div>
-
-    <div class="ajout-post">
-        <?php if($userId && $topic->getClosed() == 0) { ?>
-            <h3>ajouter un post</h3>
-            <form action="index.php?ctrl=forum&action=addPost&id=<?= $topic->getId() ?>" method="POST">
-                <textarea id="textarea" name="post" rows="1" cols="50" placeholder="ajouter votre texte ici" required></textarea>
-                <br>
-                <input type="submit" name="submit" value="envoyer" class="btn">
-            </form>
-        <?php } else if($topic->getClosed() != 0) {
-            echo "Ce topic est fermé, vous ne pouvez pas rajouter de post!";
-        } ?>
-    </div>
 <?php } ?>
+
+<?php if($userId && $topic->getClosed() == 0) { ?>
+    <div class="ajout-post">
+        <h3>ajouter un post</h3>
+        <form action="index.php?ctrl=forum&action=addPost&id=<?= $topic->getId() ?>" method="POST">
+            <textarea id="textarea" name="post" rows="1" cols="50" placeholder="ajouter votre texte ici" required></textarea>
+            <br>
+            <input type="submit" name="submit" value="envoyer" class="btn">
+        </form>
+    </div>
+<?php } else if($userId && $topic->getClosed() != 0) {
+    echo "<p>Ce topic est fermé, vous ne pouvez pas rajouter de post!</p>";
+} ?>
--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -5,59 +5,42 @@
 
 <h1>Liste des topics</h1>
 
-
 <div class="wrap">
-
-<?php
-
-foreach($topics as $topic ){ 
-
-// $idAuteur = $topic->getUser()->getNickName();
-$userId = App\Session::getUser() ? App\Session::getUser()->getId() : null;
-$role = App\Session::getUser() ? App\Session::getUser()->getRole() : null;
-// var_dump($role);die;
-//    var_dump($userId);die;
-if($userId){
-  
+    <?php
+    foreach($topics as $topic) { 
+        //on verifie si l'utilisateur est connecté on stocke son id/role dans la variable sinon on renvoie null
+        $userId = App\Session::getUser() ? App\Session::getUser()->getId() : null;
+        $role = App\Session::getUser() ? App\Session::getUser()->getRole() : null;
     ?>
-    <div class="categorie">
-        <a href="index.php?ctrl=forum&action=listPostsByTopic&id=<?= $topic->getId() ?>"><?= $topic->getTitle() ?></a>
-        par <?= $topic->getUser() ?><br>
-        le <?= $topic->getCreationDate()->format("d/m/y  H:i") ?>
-        
-        <?php
-        //je verifie si idauteur == idUser pour accorder les droit sur topic ou role s'il est admin
-            if($topic->getUser()->getId() == $userId || $role == "admin" ) { ?>
-                <?php if($topic->getClosed() == 0) { ?>
+        <div class="categorie">
+            <a href="index.php?ctrl=forum&action=listPostsByTopic&id=<?= $topic->getId() ?>"><?= $topic->getTitle() ?></a>
+            par <?= $topic->getUser() ?><br>
+            le <?= $topic->getCreationDate()->format("d/m/y H:i") ?>
+            
+            <?php
+            // Vérifie si l'auteur du topic est l'utilisateur connecté ou si l'utilisateur est un admin
+            if($userId && ($topic->getUser()->getId() == $userId || $role == "admin")) {
+                if($topic->getClosed() == 0) { ?>
                     <a href="index.php?ctrl=forum&action=closeTopic&id=<?= $topic->getId() ?>">Close</a>
-                    <?php } else { ?>
+                <?php } else { ?>
                     <a href="index.php?ctrl=forum&action=openTopic&id=<?= $topic->getId() ?>">Open</a>
                 <?php }
-            }else{
-                
             }
-        ?>
-
-    
-    <?php  ?>
-    </div>
-    <form action="index.php?ctrl=forum&action=addTopic&id=<?=$category->getId()?>" method="POST">
-        <label for="title">Titre Topic: </label>
-        <input type="text" name="title" required><br>
-
-        <label for="textarea">Premier post: </label>
-        <textarea id="textarea" name="post" rows="4" cols="50" placeholder="entrer le 1er message" required></textarea>
-    <br>
-        <input type="submit" name="submit" value="ajouter">
-        
-    </form>
-    <?php 
- }else { ?>
-        <div class="categorie">
-        <a href="index.php?ctrl=forum&action=listPostsByTopic&id=<?= $topic->getId() ?>"><?= $topic->getTitle() ?></a>
-        par <?= $topic->getUser() ?><br>
-        le <?= $topic->getCreationDate()->format("d/m/y  H:i") ?>
-    
+            ?>
         </div>
-   <?php } }?>
+    <?php } ?>
+
+    <?php if($userId) { ?>
+        <div class="ajout-topic">
+            <form action="index.php?ctrl=forum&action=addTopic&id=<?= $category->getId() ?>" method="POST">
+                <label for="title">Titre Topic: </label>
+                <input type="text" name="title" required><br>
+
+                <label for="textarea">Premier post: </label>
+                <textarea id="textarea" name="post" rows="4" cols="50" placeholder="entrer le 1er message" required></textarea>
+                <br>
+                <input type="submit" name="submit" value="ajouter">
+            </form>
+        </div>
+    <?php } ?>
 </div>
